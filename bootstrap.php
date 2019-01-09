8f391920b2331952a546e3425c7c4603d0ec09e9
t2421 <t2421gm@gmail.com>
2019-01-09 20:31:24 +0900

    共通レイアウトのダミーデータ追加

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/data/common_data.php';
+
 $loader = new Twig_Loader_Filesystem(__DIR__.'/templates');
 $twig = new Twig_Environment($loader);
 
@@ -10,7 +12,9 @@ function render($html,$data=array()){
 
 function renderContents($html,$data=array()){
     global $twig;
-    $common = $twig->render("layout.html",$data);
+    global $common_data;
+    
+    $common = $twig->render("layout.html",$common_data);
     $contents = $twig->render($html,$data);
     $header_custom = $twig->render("header_custom.html",$data);
     $body_custom = $twig->render("body_custom.html",$data);
